5f1afa094ce8b43478e587dcfe9c96a79dfb1148
Mitchell Phillips <phillips.mitchell22@yahoo.com>
2016-03-21 09:51:15 -0600

    Initial Commit

---
+++ SimpleSearchBar/SimpleSearchBar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimpleSearchBar
+//
+//  Created by Mitchell Phillips on 3/21/16.
+//  Copyright © 2016 Wasted Potential LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
